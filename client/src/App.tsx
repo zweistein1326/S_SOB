bdc742e5d8b9e2b25fa1a84f10ca3cc8f08ee913
ivan0313 <ivan0313@connect.hku.hk>
2021-11-26 23:16:40 +0800

    feat(client): added home page and connected login to api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { RestLink } from 'apollo-link-rest';
 import Login from './pages/Login';
+import Home from './pages/Home';
+
+const restLink = new RestLink({ uri: "/api" });
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: restLink
+});
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import { Button, Typography } from '@mui/material';
+import { useState } from 'react';
+
+const Home = () => {
+	const [loggedIn, setLoggedIn] = useState(localStorage.getItem('token') !== null)
+	
+	const handleLogout = () => {
+		localStorage.removeItem('token');
+		setLoggedIn(false);
+	}
+
+	return (
+		<>
+			<Typography variant='h2'>
+				Home Page
+			</Typography>
+			<Typography>
+				You are currently {loggedIn ? '' : 'NOT'} logged-in
+			</Typography>
+
+			<Button href='/login'>
+				Login
+			</Button>
+
+			<Button onClick={handleLogout}>
+				Logout
+			</Button>
+		</>
+	)
+}
+
+export default Home;
