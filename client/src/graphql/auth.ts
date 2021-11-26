2f3a4dca6438a552275150c90a01bb50ab487175
ivan0313 <ivan0313@connect.hku.hk>
2021-11-27 00:42:51 +0800

    refactor: login page

---
+++ client/src/graphql/auth.ts
@@ -0,0 +1,15 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+	fragment Payload on REST {
+    email: String
+		password: String
+  }
+	mutation Login ($input: Payload!) {
+    login (input: $input) @rest(type: "Post", method: "Post" path: "/login") {
+      status
+			token
+			message
+    }
+  }
+`
