8a5aa0d79645d09d19ee0bea1daba2a1f2271f28
zweistein1326 <sid.agarwal45@gmail.com>
2022-01-01 20:00:05 +0800

    add fields to card

--- oneID_client/src/components/User/UserCard.tsx
+++ oneID_client/src/components/User/UserCard.tsx
@@ -5,25 +5,29 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const UserCard = (props:any) => {
 
+    if(props.cardInfo){
+        console.log(Object.values(props.cardInfo));
+    }
     return(
         <View style={{display:'flex',justifyContent:'space-between', flexDirection:'column', alignItems:'center', backgroundColor:'red', width:'95%', height:250, borderRadius:20, padding:20}}>
-            {!!props.card?
-            // <Text style={[styles.cardText,{color:'white'}]}>{props.card.title}</Text>
+            {!!props.cardInfo?
+            // <Text style={[styles.cardText,{color:'white'}]}>{props.cardInfo.title}</Text>
              <View style= {{display:'flex',justifyContent:'space-between', flexDirection:'row', alignItems:'center',width:'90%', height:'100%'}}>
                 <View>
                     <Text style={[styles.cardText,{color:'white'}]}>Avatar</Text>
                 </View>
-                <View style={{width:'50%'}}>
+                 <View style={{width:'50%'}}>
                     <Text style={{...styles.cardText,fontSize:16, color:'white'}}>@{props.user.username} </Text>
-                    <Text style={{...styles.cardText,fontSize:20, color:'white'}}>{props.user.firstname} {props.user.lastname} </Text>
+                    <Text style={{...styles.cardText,fontSize:16, color:'white'}}>Fullname: {props.user.firstname} {props.user.lastname} </Text>
+                {Object.values<String>(props.cardInfo).map((info:String,index)=><Text style={{...styles.cardText,fontSize:16, color:'white'}}>{Object.keys(props.cardInfo)[index]}: {info}</Text>)}
                 </View>
                 {/* <Text>UID: </Text>
                 <Text>UID: </Text> */}
             </View> : 
             null}
             {!props.customize ? <View style={{position:'absolute', bottom:20, right:20}}>
-                 {props.card?
-                 <TouchableOpacity onPress={()=>{props.navigation.navigate('CustomizeCard',{cardId:props.card.id}) }} style={{borderRadius:20, borderColor:'white', borderWidth:1, padding:8}}> 
+                 {props.cardInfo?
+                 <TouchableOpacity onPress={()=>{props.navigation.navigate('CustomizeCard',{cardId:props.cardInfo.id}) }} style={{borderRadius:20, borderColor:'white', borderWidth:1, padding:8}}> 
                     <Text style={{color:'white'}}>Customize</Text>
                 </TouchableOpacity>
                 :
