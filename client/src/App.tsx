0b035d2b69675c8adcf599bce8bed7fcb61a973b
raphaeleg <quark265@connect.hku.hk>
2022-01-25 20:23:02 +0800

    attempt #1
    getting analytics in firebase.js, seeing whether it records my activity onsite

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from 'react-router-dom';
 import Home from './pages/UserProfile';
 import Register from './pages/Register';
 import { useEffect, useState } from 'react';
@@ -9,71 +14,77 @@ import Header from './components/Header';
 import './styles/index.css';
 import Feed from './pages/Feed';
 import NFTScreen from './pages/CredentialScreen';
-import { getAllUsers, getCredentials, getUserById, register } from './functions/axios';
+import {
+  getAllUsers,
+  getCredentials,
+  getUserById,
+  register,
+} from './functions/axios';
 import SettingsScreen from './pages/SettingsScreen';
-import {uploadBytes} from 'firebase/storage';
+import { uploadBytes } from 'firebase/storage';
 import WalletConnect from '@walletconnect/client';
 import QRCodeModal from '@walletconnect/qrcode-modal';
 import { connector } from './functions/walletConnector';
 import { setUser } from './redux/actions/user';
 
+// import * as firebase from 'firebase/app';
+// import { firebaseConfig } from './firebase/firebase';
+
 declare var window: any;
 
-const {ethereum} = window;
+const { ethereum } = window;
 
 const connectWalletHandler = async () => {
-  try{
-    const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+  try {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
     console.log('account', accounts[0]);
     console.log("Wallet exists! We're ready to go!");
-  } catch(err){
+  } catch (err) {
     console.log(err);
   }
-}
+};
 
 export const store = configureStore();
-  
-function App() {  
 
+function App() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     // connectWalletHandler();
-    var userId:any;
-    try{
-      userId = window.localStorage.getItem('userId')
-    }
-    catch(e){
+    var userId: any;
+    try {
+      userId = window.localStorage.getItem('userId');
+    } catch (e) {
       console.log(e);
     }
-    (async()=>{
-          store.dispatch(getAllUsers());
-          store.dispatch(getCredentials());
-          if(userId){
-            const user:any = await store.dispatch(getUserById(userId))
-            console.log(user);
-            if(!!user){
-              store.dispatch(setUser(user.user));
-              setIsLoggedIn(true);
-            }
-          }
-        })()
-  },[])
+    (async () => {
+      store.dispatch(getAllUsers());
+      store.dispatch(getCredentials());
+      if (userId) {
+        const user: any = await store.dispatch(getUserById(userId));
+        console.log(user);
+        if (!!user) {
+          store.dispatch(setUser(user.user));
+          setIsLoggedIn(true);
+        }
+      }
+    })();
+  }, []);
 
   return (
-      <Provider store={store}>
-        <Router>
-          <Routes>
-            <Route path="/" element={isLoggedIn ? <Feed/>:<Register/>} />
-            <Route path="/:address" element={<Home />} />
-            <Route path="/settings" element={<SettingsScreen />} />
-            <Route path="/feed" element={<Feed />} />
-            <Route path="/credential/:credentialId" element={<NFTScreen />} />
-            <Route path="/addCredential" element={<AddCredential />} />
-          </Routes>
-        </Router>
-      </Provider>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <Feed /> : <Register />} />
+          <Route path="/:address" element={<Home />} />
+          <Route path="/settings" element={<SettingsScreen />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/credential/:credentialId" element={<NFTScreen />} />
+          <Route path="/addCredential" element={<AddCredential />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
