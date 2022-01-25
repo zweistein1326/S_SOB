db4a9b245de26dcb6ada7c173eec13aa3843ea1e
raphaeleg <quark265@connect.hku.hk>
2022-01-25 19:42:24 +0800

    revert_previous_commits
    didn't realise github desktop committed old code sorry

--- oneID_client/src/constants/Constants.js
+++
@@ -1,4 +0,0 @@
-/** @format */
-
-export const baseUrl = 'http://192.168.1.110:8000/api';
-export const testUrl = 'http://192.168.1.110:8000/';
--- oneID_client/src/redux/actions/AuthActions.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-import { AsyncStorage } from '@react-native-async-storage/async-storage';
-
-export const setUser = (user) => ({
-	type: 'SET_USER',
-	payload: user,
-});
